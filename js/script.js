ba7d89b66c81a8f91a984b55f235abaf38df5680
ellavrubel <v.sukhoff@gmail.com>
2019-12-20 13:59:15 +0300

    slide

---
+++ js/script.js
@@ -0,0 +1,14 @@
+    $(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        smartSpeed:2000,
+
+        responsive:{
+            0: {
+                items: 1
+            }
+        }
+    })
+});
