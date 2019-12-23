3fe72630e167935407406b58ef71e1dfb1b94f65
innerfly <v.sukhoff@csgo.com>
2019-12-23 17:43:08 +0300

    @media 768-576

--- js/script.js
+++ js/script.js
@@ -11,4 +11,10 @@
             }
         }
     })
+
+
+        $('.header__burger').click(function (event) {
+            $('.header__burger,.header__nav').toggleClass('active');
+            $('body').toggleClass('lock');
+        })
 });
